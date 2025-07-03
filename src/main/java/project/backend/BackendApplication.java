e893a9004033760d3b78bb7d9cfeb4bdfea5aecd
yikanghee <yikanghee2@naver.com>
2025-07-03 14:55:07 +0900

    feature: [에러해결] script 수정 3차

--- src/main/java/project/backend/BackendApplication.java
+++ src/main/java/project/backend/BackendApplication.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 public class BackendApplication {
 
     public static void main(String[] args) {
+        System.out.println("🔍 DB_HOST: " + System.getenv("DB_HOST"));
+        System.out.println("🔍 DB_USERNAME: " + System.getenv("DB_USERNAME"));
         SpringApplication.run(BackendApplication.class, args);
     }
 
